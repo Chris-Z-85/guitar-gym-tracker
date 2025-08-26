90a70c9030a368f61ebcd905b9a6549762cf68d3
Krzysztof Zbrojkiewicz <chris.z@aol.com>
2025-08-26 15:58:28 +0200

    add logo

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,6 +1,7 @@
 import { PracticeGoalForm, PracticeGoal } from "@/components/PracticeGoal"
 import { useState } from "react"
 import PracticeTimer from "@/components/PracticeTimer"
+import GG_logo from "@/components/GG_logo";
 
 export default function HomePage() {
   const [currentGoal, setCurrentGoal] = useState<PracticeGoal | null>(null);
@@ -12,10 +13,18 @@ export default function HomePage() {
   return (
     <div>
       <div className="flex flex-col gap-4">
-        <div>
-          <h1 className="text-2xl font-bold text-foreground sm:text-3xl lg:text-4xl">Guitar Gym</h1>
-          <p className="text-base">
-          Welcome to the Guitar Practice Tracker. Track your daily guitar practice sessions, add exercises, and monitor your progress over time.</p>
+        <div className="flex flex-col items-center gap-4">
+          <div className="flex items-center gap-2 text-5xl font-bold">
+            <h1>GUITAR</h1>
+            <GG_logo />
+            <h1>GYM</h1>
+          </div>
+          <div className="text-center">
+            <h1 className="text-2xl font-bold text-foreground sm:text-3xl lg:text-2xl">Practice Tracker</h1>
+            <p className="text-base">
+              Track your daily guitar practice sessions, add exercises, and monitor your progress over time.
+            </p>
+          </div>
         </div>
         {currentGoal ? (
           <>
